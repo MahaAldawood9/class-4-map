f5558c00075f5f5015a0742040b6f78b6d13a183
MahaAldawood9 <ma8279@nyu.edu>
2025-04-09 09:43:54 -0400

    fixed colors and coordinates

--- data.js
+++ data.js
@@ -4,80 +4,86 @@ const buildingsData = [
         "architect": "Philip Johnson & Richard Foster",
         "year": "1973",
         "location": "Greenwich Village",
-        "latitude": 40.7295,
-        "longitude": -73.9965
+        "latitude": 40.7296301,
+        "longitude": -73.9970777
     },
+
+    
+
     {
         "name": "Ford Foundation Center for Social Justice",
         "architect": "Kevin Roche",
         "year": "1967",
         "location": "Midtown East",
-        "latitude": 40.7517,
-        "longitude": -73.9692
+        "latitude": 40.7499806,
+        "longitude": -73.9710477
     },
+
     {
         "name": "Hunter College Cooperman Plaza (North Building)",
         "architect": "Ulrich Franzen",
         "year": "1984",
         "location": "Upper East Side",
-        "latitude": 40.7683,
-        "longitude": -73.9646
+        "latitude": 40.7677283,
+        "longitude": -73.9638709
     },
+   
+
     {
         "name": "Jamaica Center for Arts & Learning",
         "architect": "NYC Department of Public Works",
         "year": "1970s",
         "location": "Queens",
-        "latitude": 40.7027,
-        "longitude": -73.7999
+        "latitude": 40.7038452,
+        "longitude": -73.7979475
     },
+
+ 
+
+
     {
         "name": "Prentis Hall (Columbia University)",
         "architect": "Harrison & Abramovitz",
         "year": "1951",
         "location": "Morningside Heights",
-        "latitude": 40.8125,
-        "longitude": -73.9619
+        "latitude": 40.8166529,
+        "longitude": -73.9594121
     },
+
+
     {
         "name": "Martin Luther King Jr. High School Complex",
         "architect": "Eggers & Higgins",
         "year": "1975",
         "location": "Upper West Side",
-        "latitude": 40.7742,
-        "longitude": -73.9850
+        "latitude": 40.7745233,
+        "longitude": -73.9849809
     },
 
+
+
+
+
     {
-        "name": "PSC-CUNY Office Building (555 W 57th St)",
-        "architect": "Holabird & Root",
-        "year": "1970s",
-        "location": "Midtown West",
-        "latitude": 40.7712,
-        "longitude": -73.9891
-    },
-    {
-        "name": "Whitney Museum (now Met Breuer)",
+        "name": "The Met Breuer",
         "architect": "Marcel Breuer",
         "year": "1966",
         "location": "Upper East Side",
-        "latitude": 40.7761,
-        "longitude": -73.9633
+        "latitude": 40.7735011,
+        "longitude": -73.9637972
     },
+
+  
+
     {
         "name": "Chatham Towers",
         "architect": "Kelly & Gruzen",
         "year": "1964",
         "location": "Chinatown",
-        "latitude": 40.7131,
-        "longitude": -74.0017
+        "latitude": 40.7137452,
+        "longitude": -74.0000365
     },
-    {
-        "name": "Clinton Hill Co-ops",
-        "architect": "Harrison & Abramovitz",
-        "year": "1957",
-        "location": "Brooklyn",
-        "latitude": 40.6884,
-        "longitude": -73.9665
-    }
+
+
+
 ];
--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-
-
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFoYWFsZGF3b29kIiwiYSI6ImNtOThyeDFzYTA2NjEya3B3ZmVkbnBlNzUifQ.a2NLZb3e_7Ezo1smY7SpMQ';
 
 const mapOptions = {
@@ -9,24 +7,21 @@ const mapOptions = {
     zoom: 10,
 };
 
-
 const map = new mapboxgl.Map(mapOptions);
 
-
 const decadeColors = {
-    '1950': '#744800',
-    '1960': '#741000',
-    '1970': '#740074',
-    '1980': '#1b0074'
+    '1950s': '#744800',
+    '1960s': '#741000',
+    '1970s': '#740074',
+    '1980s': '#1b0074'
 };
 
 function getDecade(year) {
-    ;
+    if (year < 1960) return '1950s';
     if (year < 1970) return '1960s';
     if (year < 1980) return '1970s';
     return '1980s';
 }
-
 
 buildingsData.forEach((building) => {
     const popup = new mapboxgl.Popup({ offset: 36 })
@@ -38,13 +33,17 @@ buildingsData.forEach((building) => {
         `);
 
 
-    const buildingColor = getDecade(parseInt(building.year));
+    const buildingYear = parseInt(building.year);
+    const buildingDecade = getDecade(buildingYear); 
+
+
+    const buildingColor = decadeColors[buildingDecade] || '#000000'; 
+
 
     new mapboxgl.Marker({
-        scale: 0.8,
-        color: buildingColor
+        color: buildingColor 
     })
         .setLngLat([building.longitude, building.latitude])
         .setPopup(popup)
         .addTo(map);
-})
+});
