4c75f639637a7c66b1399fe9ddc0f5e36e7103a3
MahaAldawood9 <ma8279@nyu.edu>
2025-04-22 10:55:49 -0400

    draft 1

--- data.js
+++ data.js
@@ -1,89 +1,127 @@
-const buildingsData = [
-    {
-        "name": "Bobst Library (NYU)",
-        "architect": "Philip Johnson & Richard Foster",
-        "year": "1973",
-        "location": "Greenwich Village",
-        "latitude": 40.7296301,
-        "longitude": -73.9970777
-    },
-
-    
-
-    {
-        "name": "Ford Foundation Center for Social Justice",
-        "architect": "Kevin Roche",
-        "year": "1967",
-        "location": "Midtown East",
-        "latitude": 40.7499806,
-        "longitude": -73.9710477
-    },
-
-    {
-        "name": "Hunter College Cooperman Plaza (North Building)",
-        "architect": "Ulrich Franzen",
-        "year": "1984",
-        "location": "Upper East Side",
-        "latitude": 40.7677283,
-        "longitude": -73.9638709
-    },
-   
-
-    {
-        "name": "Jamaica Center for Arts & Learning",
-        "architect": "NYC Department of Public Works",
-        "year": "1970s",
-        "location": "Queens",
-        "latitude": 40.7038452,
-        "longitude": -73.7979475
-    },
-
- 
-
-
-    {
-        "name": "Prentis Hall (Columbia University)",
-        "architect": "Harrison & Abramovitz",
-        "year": "1951",
-        "location": "Morningside Heights",
-        "latitude": 40.8166529,
-        "longitude": -73.9594121
-    },
-
-
-    {
-        "name": "Martin Luther King Jr. High School Complex",
-        "architect": "Eggers & Higgins",
-        "year": "1975",
-        "location": "Upper West Side",
-        "latitude": 40.7745233,
-        "longitude": -73.9849809
-    },
+const randomPoints =
+{
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    -73.98390182137894,
+                    40.72664210693773
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    -73.95282010028582,
+                    40.708645102023326
+                ],
+                "type": "Point"
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {},
+            "geometry": {
+                "coordinates": [
+                    -73.9973750137844,
+                    40.69307073415601
+                ],
+                "type": "Point"
+            }
+        }
+    ]
+}
 
 
 
-
-
-    {
-        "name": "The Met Breuer",
-        "architect": "Marcel Breuer",
-        "year": "1966",
-        "location": "Upper East Side",
-        "latitude": 40.7735011,
-        "longitude": -73.9637972
-    },
-
-  
-
-    {
-        "name": "Chatham Towers",
-        "architect": "Kelly & Gruzen",
-        "year": "1964",
-        "location": "Chinatown",
-        "latitude": 40.7137452,
-        "longitude": -74.0000365
-    },
-
-
-
-];
+const governorsislandAura = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            -73.98390182137894,
+            40.72664210693773
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            -73.95282010028582,
+            40.708645102023326
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            -73.9973750137844,
+            40.69307073415601
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "coordinates": [
+            [
+              [
+                -74.02314568096834,
+                40.689484095290084
+              ],
+              [
+                -74.02775448490667,
+                40.685420431792835
+              ],
+              [
+                -74.02400313286402,
+                40.68371361923252
+              ],
+              [
+                -74.01767942513494,
+                40.6873710211961
+              ],
+              [
+                -74.01221316930092,
+                40.68842756661826
+              ],
+              [
+                -74.01210598781452,
+                40.690784415275175
+              ],
+              [
+                -74.01446398052688,
+                40.69297864771207
+              ],
+              [
+                -74.01971587338683,
+                40.6928973811325
+              ],
+              [
+                -74.02314568096834,
+                40.689484095290084
+              ]
+            ]
+          ],
+          "type": "Polygon"
+        }
+      }
+    ]
+  }
